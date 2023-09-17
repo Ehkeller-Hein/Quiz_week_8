8b9ff4e164a56f0178ee3c575b7ec494866728a0
Ehkeller-Hein <ehkellerhein14@gmail.com>
2023-09-17 13:45:38 +1000

    update_q2.py
    updated q2

--- climate_q2.py
+++ climate_q2.py
@@ -2,10 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load data from the CSV file
-data = pd.read_csv('climate.csv')  # Replace 'your_data.csv' with the actual CSV file path
+data = pd.read_csv('climate.csv')
 
-# Assuming your CSV file has columns named 'Date' and 'Temperature'
-# You can modify these column names based on your data
+
 years = data['Year']
 cO2 = data['CO2']
 temperatures = data['Temperature']
@@ -13,14 +12,19 @@ temperatures = data['Temperature']
 # Create a figure and plot the data
 plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
 
-plt.plot(years, temperatures, marker='o', linestyle='-', color='b')
-plt.title('ClimateData')
-plt.xlabel('Year')
-plt.ylabel('Temperature (°C)')
-plt.grid(True)
+plt.plot(years, cO2, marker='o', linestyle='-', color='b')
+plt.subplot(2, 1, 1)
+plt.plot(years, cO2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
+plt.subplot(2, 1, 2)
+plt.plot(years, temperatures, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
 
-# Rotate the x-axis labels for better readability (optional)
+# Rotate the x-axis labels for better readability
 plt.xticks(rotation=45)
 
 # Show the plot
